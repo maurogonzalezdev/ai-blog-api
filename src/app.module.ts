cd9f6ef3d52d3403647929c8d6883e946f56996a
Mauricio González <hello@mauro-gonzalez.dev>
2025-03-14 20:07:46 -0300

    feat(config): setup ConfigModule for root in NestJS application
    - Installed @nestjs/config package
    - Configured ConfigModule with forRoot method
    - Enabled environment variable management for application-wide settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })

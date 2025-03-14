4926b393e894b6c3ff4096a48ea9a7e086cb7d05
Mauricio González <hello@mauro-gonzalez.dev>
2025-03-14 19:26:06 -0300

    feat(config): setup environment variables and configure global settings
    - Added .env.example file
    - Installed class-validator and class-transformer packages
    - Set global prefix to 'api/v1' using app.setGlobalPrefix
    - Enabled global validation pipes with whitelist, forbidNonWhitelisted, and transform options

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
   );
   await app.listen(process.env.PORT ?? 3000);
 }

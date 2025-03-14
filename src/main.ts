1aaef8bdf481865e2a0616c30d7a3a87fcd18c64
Mauricio González <hello@mauro-gonzalez.dev>
2025-03-14 20:07:46 -0300

    feat(config): setup ConfigModule for root in NestJS application
    - Installed @nestjs/config package
    - Configured ConfigModule with forRoot method
    - Enabled environment variable management for application-wide settings

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(

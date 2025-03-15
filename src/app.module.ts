33e9b2adfadf310a7802ef75bfe6fcdd590d7c9d
Mauricio González <hello@mauro-gonzalez.dev>
2025-03-14 21:42:52 -0300

    feat(database): integrate Prisma into the project
    - Integrated Prisma ORM into the application
    - Created initial database migration
    - Added Prisma module and Prisma service with Prisma Client
    - Established connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { PrismaModule } from '@app/prisma/prisma.module';
+
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
